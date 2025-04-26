a960379d233558b515a33a9179a3937fbdd4b4aa
Satya Ajay <rsa914016@gmail.com>
2025-04-26 22:41:04 +0530

    feat: removed all resources

--- GCP/buckets.tf
+++ GCP/buckets.tf
@@ -1,18 +1,18 @@
 // PDF Bucket
-module "pdfs_bucket" {
-  source                      = "./tf-modules/gcs-bucket"
-  bucket_name                 = "transcript-pdfs"
-  bucket_location             = var.region
-  uniform_bucket_level_access = true
-  block_public_access         = true
-  lifecycle_rules = [{
-    action = {
-      type = "Delete"
-    }
-    condition = {
-      age            = 100
-      with_state     = "ANY"
-      matches_suffix = [".pdf"]
-    }
-  }]
-}
+# module "pdfs_bucket" {
+#   source                      = "./tf-modules/gcs-bucket"
+#   bucket_name                 = "transcript-pdfs"
+#   bucket_location             = var.region
+#   uniform_bucket_level_access = true
+#   block_public_access         = true
+#   lifecycle_rules = [{
+#     action = {
+#       type = "Delete"
+#     }
+#     condition = {
+#       age            = 100
+#       with_state     = "ANY"
+#       matches_suffix = [".pdf"]
+#     }
+#   }]
+# }
--- GCP/iam-sa.tf
+++ GCP/iam-sa.tf
@@ -1,21 +1,21 @@
 // Cloud Run Service Account
-module "cloud-run-sa" {
-  source             = "./tf-modules/iam-sa"
-  service_account_id = "cloud-run-sa"
-  display_name       = "Cloud Run Service Account"
-  iam_roles = [
-    "roles/run.admin",
-    "roles/storage.objectViewer"
-  ]
-}
+# module "cloud-run-sa" {
+#   source             = "./tf-modules/iam-sa"
+#   service_account_id = "cloud-run-sa"
+#   display_name       = "Cloud Run Service Account"
+#   iam_roles = [
+#     "roles/run.admin",
+#     "roles/storage.objectViewer"
+#   ]
+# }
 
-// Grafana Service Account
-module "grafana-sa" {
-  source             = "./tf-modules/iam-sa"
-  service_account_id = "grafana-sa"
-  display_name       = "Grafana Service Account"
-  iam_roles = [
-    "roles/monitoring.viewer",
-    "roles/logging.viewer",
-  ]
-}
+# // Grafana Service Account
+# module "grafana-sa" {
+#   source             = "./tf-modules/iam-sa"
+#   service_account_id = "grafana-sa"
+#   display_name       = "Grafana Service Account"
+#   iam_roles = [
+#     "roles/monitoring.viewer",
+#     "roles/logging.viewer",
+#   ]
+# }
